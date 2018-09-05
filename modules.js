3f6f0e23bf32c6500ad80da1116a40069e3e3f0f
leosteil <lsteil@inf.ufsm.br>
2018-09-05 00:25:31 -0300

    Refatoração dos módulos, adição de modal para cadastro

--- modules.js
+++ modules.js
@@ -1,58 +1,72 @@
 var dataController = (function () {
 
-	var getData = function (callback, url) {
-		var request = new XMLHttpRequest();
-		
-		request.onreadystatechange = function() {
-		 	if(request.readyState === 4) {
-		    	if(request.status === 200) {
-		    		var obj = JSON.parse(request.responseText);
-		    		callback(obj);
-		    	} else {
-		      		console.log('An error occurred during your request: ' +  request.status + ' ' + request.statusText);
-		    	} 
-		  	}
-		}
-
-		request.open('Get', url);
-		request.send();
+	var Item = function(tipoTitulo, tituloPrimario, tituloOriginal, anoInicio, anoFim, duracaoMinutos, generos){
+		this.tipoTitulo = tipoTitulo;
+		this.tituloPrimario = tituloPrimario;
+		this.tituloOriginal = tituloOriginal;
+		this.anoInicio = anoInicio;
+		this.anoFim = anoFim;
+		this.duracaoMinutos = duracaoMinutos;
+		this.generos = generos;
 	}
 
 	return {
-		filmes: function (callback){
-			return getData(callback, 'http://localhost:8080/api/filmes/pagina/1');
-		},
+		getData: function(callback, url){
+			var request = new XMLHttpRequest();
+			
+			request.onreadystatechange = function() {
+			 	if(request.readyState === 4) {
+			    	if(request.status === 200) {
+			    		var obj = JSON.parse(request.responseText);
+			    		callback(obj);
+			    	} else {
+			    		callback(false);
+			    	} 
+			  	}
+			}
 
-		series: function(callback){
-			return getData(callback, 'http://localhost:8080/api/series/pagina/1');
-		},
-
-		curtas: function(callback){
-			return getData(callback, 'http://localhost:8080/api/curtas/pagina/1');
+			request.open('Get', url);
+			request.send();
 		}
 	};
 
 })();
 
 var UIController = (function () {
+	
 	var tablesREF = {
 		tabela_filmes : "tabela-filmes",
 		tabela_series : "tabela-series",
 		tabela_curtas : "tabela-curtas"
 	};
 
+	var DOMstrings = {
+		tipo_titulo : "tipoTitulo",
+		titulo_original : "tituloOriginal",
+		titulo_primario : "tituloPrimario",
+		duracao : "duracao",
+		ano_inicio : "anoInicio",
+		ano_fim : "anoFim",
+		generos: "generos",
+
+		btn_cadastro : 'btn-cadastra',
+		btn_deleta : 'btn btn-danger btn-deleta'
+	}
 
 	var newBtn = function (trNova, btnClass, btnText, btnElemId){
 		var td, txt, btn;
 
 		td = document.createElement("td");
-		
 		btn = document.createElement("button");
+		
 		btn.type = "button";
 		
-		btn.setAttribute("data-toggle", "modal");
-		btn.setAttribute("data-target", "#exampleModal");
-		btn.setAttribute("elem-id", "btnElemId");
+
+		if(btnText != "Deletar"){
+			btn.setAttribute("data-toggle", "modal");
+			btn.setAttribute("data-target", "#exampleModal");
+		}
+		btn.setAttribute("elem-id", btnElemId);
 
 		txt = document.createTextNode(btnText);
 		btn.appendChild(txt);
@@ -72,39 +86,36 @@ var UIController = (function () {
 		trNova.appendChild(td);
 	}
 
-
-	var preencheTabela = function(movies, tableRef){
-
-		for(i = 0; i < movies.length; i++){
-			var trNova = document.createElement("tr");
-
-
-			Object.keys(movies[i]).forEach(function(key) {
-	    		if(key == 'tituloPrimario'){
-					newTD(trNova, movies[i][key]);		
-	    		}
-	    		
-	    		if(key == 'tituloOriginal'){
-	    			newTD(trNova, movies[i][key]);
-	    		}
-
-			});
-
-			newBtn(trNova, "btn btn-primary btn-modal", "Info", movies[i]._id);
-			newBtn(trNova, "btn btn-danger", "Deletar");
-
-			document.getElementById(tableRef).getElementsByTagName('tbody')[0].appendChild(trNova);
-		}
-	}
-
 	return {
 
 		getTablesREF: function() {
 			return tablesREF;
 		},
 
+		getDOMstrings: function() {
+			return DOMstrings;
+		},
+
 		preencheTabela: function(movies, tableRef){
-			preencheTabela(movies, tableRef);
+			for(i = 0; i < movies.length; i++){
+				var trNova = document.createElement("tr");
+
+				Object.keys(movies[i]).forEach(function(key) {
+		    		if(key == 'tituloPrimario'){
+						newTD(trNova, movies[i][key]);		
+		    		}
+		    		
+		    		if(key == 'tituloOriginal'){
+		    			newTD(trNova, movies[i][key]);
+		    		}
+
+				});
+
+				newBtn(trNova, "btn btn-primary btn-modal", "Info", movies[i]._id);
+				newBtn(trNova, "btn btn-danger btn-deleta", "Deletar", movies[i]._id);
+
+				document.getElementById(tableRef).getElementsByTagName('tbody')[0].appendChild(trNova);
+			}
 		}
 
 	};
@@ -113,34 +124,60 @@ var UIController = (function () {
 
 
 var controller = (function (dataCtrl, UICtrl) {
-	var DOM = UICtrl.getTablesREF();
+	var DOMtables = UICtrl.getTablesREF();
+	var DOMstrings = UICtrl.getDOMstrings();
 
-	var preencheFilmes = function(){
-		var obj = dataCtrl.filmes(function(obj){
-			UICtrl.preencheTabela(obj, DOM.tabela_filmes);
-		});
+	var preencheTabelas = function(){
+		var obj;
+
+		obj = dataCtrl.getData(function(obj){			
+			obj ? UICtrl.preencheTabela(obj, DOMtables.tabela_filmes) : alert("ERRO");
+		}, "http://localhost:8080/api/filmes/pagina/1");		
+	
+		obj = dataCtrl.getData(function(obj){			
+			obj ? UICtrl.preencheTabela(obj, DOMtables.tabela_series) : alert("ERRO");
+		}, "http://localhost:8080/api/series/pagina/1");
+
+		obj = dataCtrl.getData(function(obj){			
+			obj ? UICtrl.preencheTabela(obj, DOMtables.tabela_curtas) : alert("ERRO");
+		}, "http://localhost:8080/api/curtas/pagina/1");
 	}
 
-	var preencheSeries = function(){
-		var obj = dataCtrl.series(function(obj){
-			UICtrl.preencheTabela(obj, DOM.tabela_series);
-		});
+	var	criaEventListeners = function() {
+		document.getElementById(DOMstrings.btn_cadastro).addEventListener('click', ctrlAddItem);
+
+		var btn_del = document.getElementsByClassName(DOMstrings.btn_deleta);
+
+		console.log(btn_del.length);
+
+		for (var i = 0; i < btn_del.length; i++){
+			console.log("jdhksjdf");
+			btn_del[i].addEventListener('click', crtlDeleteItem);
+		}
+
 	}
 
-	var preencheCurtas = function(){
-		var obj = dataCtrl.curtas(function(obj){
-			UICtrl.preencheTabela(obj, DOM.tabela_curtas);
-		});
+	var ctrlAddItem = function() {
+
 	}
 
+	var crtlDeleteItem = function(){
+		//1. remover elemento do banco via datactrl
+
+		//2. remover elemento da tabela via uictrl
+
+
+		console.log("Leonardo");
+	}
 
 
 	return {
 		init: function(){
 			console.log("APP START");
-			preencheFilmes();
-			preencheSeries();
-			preencheCurtas();
+			
+			preencheTabelas();
+			
+			criaEventListeners();
 		}
 	};
 
